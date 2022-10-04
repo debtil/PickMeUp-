f011f7ffa910529c6eb71cf86c1358071aba44af
debtil <75885796+debtil@users.noreply.github.com>
2022-10-04 11:54:22 -0300

    initial commit

---
+++ src/app/services/usuario-fire-base.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { 
+  addDoc, 
+  collection, 
+  collectionData, 
+  doc, 
+  updateDoc,
+  deleteDoc, 
+  Firestore 
+} from '@angular/fire/firestore'
+import { Observable } from 'rxjs';
+
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+//import {Usuario} from '../models/Usuario';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioFireBaseService {
+  private PATH : string = "usuarios";
+
+  constructor(private angularFirestore: Firestore) { }
+
+  inserirUsuario(usuario: Usuario) {
+    usuario.id = doc(collection(this.angularFirestore, 'id')).id
+    return addDoc(collection(this.angularFirestore, this.PATH), usuario)
+  }
+
+  readUser(): Observable<Usuario[]> {
+    let prodRef = collection(this.angularFirestore, this.PATH)
+    return collectionData(prodRef, {idField: 'id'}) as Observable<Usuario[]>
+  }
+}
